7151bb4158f83606c87450e6bc85b9084723d8e7
LoganTresslerGCU <113136223+LoganTresslerGCU@users.noreply.github.com>
2025-02-09 13:02:40 -0700

    Organization - Deleting

--- FireboxFrontend/app/Home.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Text } from 'react-native';
-
-export default function HomeScreen() {
-    return (
-        <Text>You're home! You logged in or registered successfully!</Text>
-    );
-}
